4feb4234beb13182c8ac3477ab2a1548958ad13f
Tomasz Plewniak <Tomasz_Plewniak@epam.com>
2020-10-05 20:07:54 +0200

    Revert "Revert "daw""
    This reverts commit 0287a3f7d8eb8b4f214d94e30e242d3bc3b49599.

---
+++ Problem Solving/Simple Array Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
